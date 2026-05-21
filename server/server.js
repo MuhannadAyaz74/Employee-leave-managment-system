b59d18a2a8dab5847ff3cfe398e1fc784bf236c6
abdul Raheem <abdulraheemkhan1123@gmail.com>
2026-05-21 14:48:18 +0500

    update

--- server/server.js
+++ server/server.js
@@ -13,6 +13,10 @@ const adminRoutes = require('./routes/adminRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+
+
+//This the middleware
 // Middleware
 app.use(cors());
 app.use(express.json());
